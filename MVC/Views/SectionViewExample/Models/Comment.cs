f3fb4791d535331928e4d2e80c46abbd1d6c0764
sujoncep <sujoncep@gmail.com>
2024-11-21 11:58:28 +0600

    ViewStart and Layout

---
+++ MVC/Views/SectionViewExample/Models/Comment.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SectionViewExample.Models
+{
+    public class Comment
+    {
+        public string User { get; set; }
+        public string Text { get; set; }
+    }
+}
---
+++ MVC/Views/SectionViewExample/Models/CommentService.cs
@@ -0,0 +1,16 @@
+﻿namespace SectionViewExample.Models
+{
+    public class CommentService
+    {
+        public static async Task<List<Comment>> GetRecentCommentsAsync()
+        {
+            await Task.Delay(500);
+            return new List<Comment>
+            {
+                new Comment(){Text = "This is a great post!", User= "Alice"},
+                new Comment(){Text = "Very informative, thanks for sharing.", User= "Bob"},
+                new Comment(){Text = "I had a similar experience.", User= "Charlie"}
+            };
+        }
+    }
+}
