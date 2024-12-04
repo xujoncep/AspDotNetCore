266f3d1838b1d3175de247afdee8da4a799b94e6
sujoncep <sujoncep@gmail.com>
2024-12-04 17:35:57 +0600

    Complete

--- Microsoft/MvcMovie/MvcMovie/Models/Movie.cs
+++ Microsoft/MvcMovie/MvcMovie/Models/Movie.cs
@@ -8,16 +8,25 @@ namespace MvcMovie.Models
     {
         [Key]
         public int Id { get; set; } 
+
+        [Required]
+        [StringLength(60, MinimumLength =5)]
         public string? Title { get; set; }
 
         [DataType(DataType.Date)]
         [Display(Name =" Release Date")]
         public DateTime ReleaseDate { get; set; }
 
+        [Required,RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
         public string Genre { get; set; }
-
+        [Range(1,100)]
+        [DataType(DataType.Currency)]
         [Column(TypeName ="decimal(18,2)")]
         public decimal Price { get; set; }
+
+        [Required,StringLength(5)]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        public string? Rating { get; set; }
 
     }
 }
